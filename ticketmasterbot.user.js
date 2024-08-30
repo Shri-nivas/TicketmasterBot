38782eb0681dba627d49c6c078d546bc5c602653
Siri <145671037+Shri-nivas@users.noreply.github.com>
2024-08-30 22:14:54 +0100

    Update ticketmasterbot.user.js

--- ticketmasterbot.user.js
+++ ticketmasterbot.user.js
@@ -1,170 +1,132 @@
 // ==UserScript==
-// @name         TicketMaster
+// @name         TicketMaster UK Auto-Booker
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Fast execution of reserving tickets in cart
-// @match        https://www1.ticketmaster.co.uk/*
-// @match        https://www1.ticketmaster.com/*
-// @match        https://www1.ticketmaster.ie/*
-// @require      https://code.jquery.com/jquery-2.1.3.min.js
+// @version      0.5
+// @description  Auto-reserve tickets in cart for TicketMaster UK
+// @match        https://www.ticketmaster.co.uk/*
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        none
 // ==/UserScript==
 
+var refreshIntervalSeconds = 2; // Interval to check for tickets
+var numberOfTickets = 2; // Number of tickets you want
 
-var refreshIntervalSeconds=1; //Set this to how often you want to check for tickets (Note: Do this too fast and TicketMaster may block your ip address)
-var numberOfTickets=2; //Set this to the number of tickets you want
-
-function SkipPopup()
-{
-    var popupPresent = getElementByXpath('//button[@class = "modal-dialog__button landing-modal-footer__skip-button"]');
-    if(popupPresent)
-    {
-        try{ popupPresent.click();}catch(ex){}
+function SkipPopup() {
+    var popupPresent = getElementByXpath('//button[contains(@class, "landing-modal-footer__skip-button")]');
+    if (popupPresent) {
+        try { popupPresent.click(); } catch (ex) { console.error('Popup skip failed', ex); }
     }
 }
 
-function CheckForFilterPanel(){
-    var filterBar = getElementByXpath('//div[@class = "filter-bar__content"]');
-    return filterBar;  
+function CheckForFilterPanel() {
+    var filterBar = getElementByXpath('//div[contains(@class, "filter-bar__content")]');
+    return filterBar;
 }
 
-function ProcessFilterPanel(filterBar){
-    //Click first ticket result in list
-    ClickElement('(//ul/li[@class = "quick-picks__list-item"])[1]/div/div');
-    
-    //Change ticket quantity (if applicable)
-    waitForElement('.offer-card', function() {
-        
-        //Change the number of tickets (if applicable);
+function ProcessFilterPanel(filterBar) {
+    // Click the first available ticket in the list
+    ClickElement('(//ul/li[contains(@class, "quick-picks__list-item")])[1]//div[contains(@class, "event-list-item--wrapper")]');
+
+    // Wait for the offer card to load and process it
+    waitForElement('.offer-card', function () {
         ChangeTicketQuantity();
-        
-        //Click the button to Buy the tickets (right hand panel)
-        var getTicketsElement = ClickElement('//button[@id = "offer-card-buy-button"]'); 
 
-        //Sometimes a dialog comes up if someone else beat us to the tickets.
-        //This dialog gives a recommendation for a new seat selection.
-        //If this occurs, we choose to accept the new seats.
-        waitForElement('.button-aux, .modal-dialog__button', function() {
-          var sectionChangeBuyButton = getElementByXpath('//button[@class = "button-aux modal-dialog__button"]');
-          sectionChangeBuyButton.click();
+        // Click the "Buy Tickets" button in the right-hand panel
+        var getTicketsElement = ClickElement('//button[@id = "offer-card-buy-button"]');
+
+        // Handle the dialog that appears if someone else beats you to the tickets
+        waitForElement('.button-aux, .modal-dialog__button', function () {
+            var sectionChangeBuyButton = getElementByXpath('//button[contains(@class, "button-aux modal-dialog__button")]');
+            if (sectionChangeBuyButton) {
+                sectionChangeBuyButton.click();
+            }
         });
-        
-
     });
 }
 
-function ChangeTicketQuantity()
-{
-        var rightPanelCurrentTicketCountElement = getElementByXpath('//div[@class = "qty-picker__number qty-picker__number--lg"]');
-        var currentTicketCount = rightPanelCurrentTicketCountElement.innerText;
+function ChangeTicketQuantity() {
+    var currentTicketCountElement = getElementByXpath('//div[contains(@class, "qty-picker__number--lg")]');
+    var currentTicketCount = parseInt(currentTicketCountElement.innerText.trim(), 10);
 
-        var ticketQuantityDifference = numberOfTickets - currentTicketCount;
-        if (ticketQuantityDifference > 0)
-        {
-            var ticketIncrementElement = ClickElement('//button[@class = "qty-picker__button qty-picker__button--increment qty-picker__button--lg"]');
-            for (var i = 0; i < ticketQuantityDifference; i++)
-            {
-                try{ticketIncrementElement.click();}catch(ex){}
-            }
+    var ticketQuantityDifference = numberOfTickets - currentTicketCount;
+    if (ticketQuantityDifference > 0) {
+        var ticketIncrementElement = getElementByXpath('//button[contains(@class, "qty-picker__button--increment")]');
+        for (var i = 0; i < ticketQuantityDifference; i++) {
+            try { ticketIncrementElement.click(); } catch (ex) { console.error('Increment click failed', ex); }
         }
-        else if(ticketQuantityDifference < 0)
-        {
-            ticketQuantityDifference = Math.abs(ticketQuantityDifference);
-            var ticketDecrementElement = ClickElement('//button[@class = "qty-picker__button qty-picker__button--decrement qty-picker__button--lg"]');
-            for (var i = 0; i < ticketQuantityDifference; i++)
-            {
-                try{ticketDecrementElement.click();}catch(ex){}
-            }
+    } else if (ticketQuantityDifference < 0) {
+        ticketQuantityDifference = Math.abs(ticketQuantityDifference);
+        var ticketDecrementElement = getElementByXpath('//button[contains(@class, "qty-picker__button--decrement")]');
+        for (var i = 0; i < ticketQuantityDifference; i++) {
+            try { ticketDecrementElement.click(); } catch (ex) { console.error('Decrement click failed', ex); }
         }
+    }
 }
 
-function CheckForGeneralAdmission()
-{
-    var BuyButton = getElementByXpath('//button[@id = "offer-card-buy-button"]');
-    return BuyButton;
+function CheckForGeneralAdmission() {
+    var buyButton = getElementByXpath('//button[@id = "offer-card-buy-button"]');
+    return buyButton;
 }
-    
-function ProcessGeneralAdmission(generalAdmissionBuyButton)
-{
+
+function ProcessGeneralAdmission(generalAdmissionBuyButton) {
     ChangeTicketQuantity();
-    generalAdmissionBuyButton.click();  
+    generalAdmissionBuyButton.click();
 }
 
 function reload() {
-    window.top.document.location.replace(window.top.document.location.href);
+    window.location.reload();
 }
 
-
-function ClickElement(path, time) 
-{
+function ClickElement(path) {
     var element = getElementByXpath(path);
-    if(element !== null) {
-        if (typeof element.click != 'undefined')
-        {
-            element.click();
-            return element;
-        }
+    if (element && typeof element.click !== 'undefined') {
+        element.click();
+        return element;
     }
 }
 
-function getElementByXpath(path) 
-{
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-var waitForElement = function(selector, callback) 
-{
-  if (jQuery(selector).length) {
-    callback();
-  } else {
-    setTimeout(function() {
-      waitForElement(selector, callback);
-    }, 100);
-  }
+var waitForElement = function (selector, callback) {
+    if (jQuery(selector).length) {
+        callback();
+    } else {
+        setTimeout(function () {
+            waitForElement(selector, callback);
+        }, 100);
+    }
 };
 
-$(document).ready(function() 
-{   
-    var success=false;
-    //This popup dialog seems to happen in the US ticketmaster website
-    //We just close it down and continue as normal
+$(document).ready(function () {
+    var success = false;
+
+    // Close any popups that might block the process
     SkipPopup();
-    
-    //Ticket type 1
-    //This occurs in the majority of ticket sales when there is a selection of ticket types
-    if(!success)
-    {
+
+    // Check if there's a filter bar to select specific tickets
+    if (!success) {
         var filterBar = CheckForFilterPanel();
-        if(filterBar)
-        {
-            console.log('These tickets have a filter bar');
-            success=true;
+        if (filterBar) {
+            console.log('Filter bar detected');
+            success = true;
             ProcessFilterPanel(filterBar);
         }
     }
-    
-    //Ticket type 2
-    //These tickets are General Admission and do not have assigned seating (i.e. no filter bar)
-    if(!success)
-    {
+
+    // Handle general admission tickets with no assigned seating
+    if (!success) {
         var generalAdmissionBuyButton = CheckForGeneralAdmission();
-        if(generalAdmissionBuyButton)
-        {
-            console.log('These tickets are General Admission');
-            success=true;
+        if (generalAdmissionBuyButton) {
+            console.log('General admission detected');
+            success = true;
             ProcessGeneralAdmission(generalAdmissionBuyButton);
         }
     }
-    
-    //TODO: Add more ticket types if found
 
-    if(!success)
-    {
-        //refresh the page after an interval (Tickets weren't yet on sale)
-        setTimeout(function(){reload();}, refreshIntervalSeconds * 1000); 
+    // If no tickets are found, refresh the page after a set interval
+    if (!success) {
+        setTimeout(reload, refreshIntervalSeconds * 1000);
     }
 });
-
-
-
-
